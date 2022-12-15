8f5c6224bf5651a019c2644d2b09415241f1d809
Georgi Ninoshev <georgi.ninoshev@gmail.com>
2022-12-15 20:18:34 +0200

    gitignore update

--- tests.py
+++ tests.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 
-interface = input("Enter interface type and number: ")
-vlan = input("Enter VLAN number: ")
+import dnspython as dns
+import dns.resolver
 
-access_template = [
-    'switchport mode acceess',
-    'switchport access vlan {}',
-    'switchport nonegotiate',
-    'spanning-tree port fast',
-    'spanning-tree bpduguard enable'
-]
+result = dns.resolver.query('google.com', 'A')
+
+print(result)
