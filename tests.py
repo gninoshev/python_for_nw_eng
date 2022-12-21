abd2d015fae54247423fe83de46423f1819caebd
Georgi Ninoshev <georgi.ninoshev@gmail.com>
2022-12-21 21:15:49 +0200

    test

--- tests.py
+++ tests.py
@@ -1,13 +1,1 @@
 #!/usr/bin/env python
-
-# import dns.resolver
-# result = dns.resolver.resolve('google.com', 'A')
-#
-# for ipval in result:
-#     print(ipval.to_text())
-#
-# print(result.nameserver)
-
-import socket
-
-print(f'The IP is {socket.gethostbyname_ex("google.bg")[2][0]}')
