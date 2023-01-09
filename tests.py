1b2b4ddf83683051360bf0076282668e6e6d3239
Georgi Ninoshev <georgi.ninoshev@gmail.com>
2023-01-09 22:00:58 +0200

    test

--- tests.py
+++ tests.py
@@ -1,1 +1,20 @@
 #!/usr/bin/env python
+
+# result = {}
+
+# with open('sh_ip_int_br.txt') as f:
+#     for line in f:
+#         line = line.split()
+#         if line and line[1][0].isdigit():
+#             interface, address, *other = line
+#             result[interface] = address
+
+# print(result)
+
+
+from itertools import product
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+print(*product(a, b))
